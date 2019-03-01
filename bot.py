e488d888a7ab592516356a2b62eb1ebf4ddae66f
Irina <lucien00@bk.ru>
2019-03-01 20:41:36 +0300

    bad version

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+import datetime
 
 
 
@@ -23,6 +25,36 @@ def talk_to_me(bot, update):
 
 
 
+
+def planets(bot, update):
+    begin = 'Напишите название одной из этих планет: Mercury, Mars, Venus, Jupiter, Saturn, Uranus, Neptune, чтобы узнать, в каком созвездии она сегодня находится: '
+    logging.info(begin)
+    update.message.reply_text(begin)
+
+
+date = datetime.datetime.now()
+
+planet = {
+    'Mercury': ephem.constellation(ephem.Mercury(date)),
+    'Mars': ephem.constellation(ephem.Mars(date)),
+    'Venus': ephem.constellation(ephem.Venus(date)),
+    'Jupiter': ephem.constellation(ephem.Jupiter(date)),
+    'Saturn': ephem.constellation(ephem.Saturn(date)),
+    'Uranus': ephem.constellation(ephem.Uranus(date)),
+    'Neptune': ephem.constellation(ephem.Neptune(date)),
+}
+
+def constellation(bot, update):
+
+    name = update.message.text
+    answer = 'Планета находится в созвездии {}'.format(planet[name])
+
+    logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username, 
+              update.message.chat.id, update.message.text)
+    update.message.reply_text(answer)
+
+
+
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     
@@ -30,7 +62,17 @@ def main():
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planets))
+
+    
+    dp.add_handler(MessageHandler(Filters.text, constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+
+    
+    
+
+    
 
     mybot.start_polling()
     mybot.idle()
